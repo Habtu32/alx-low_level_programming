405c80338d44577f2d174a9342f51812e29934f1
Habtu32 <habtuwendifraw@gmail.com>
2022-12-17 18:57:07 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - main function to print alpahbetic character
+ *
+ *Return:0
+ */
+int main(void)
+{
+	int c = '0';
+
+	for (; c < 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
