50d0eb95c1abcacaa5cbcd939895fc9dbf9e2377
Habtu32 <habtuwendifraw@gmail.com>
2022-12-17 23:08:15 -0800

    all posible combination of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - to print combination of numers
+ *
+ * Return:0
+ *
+ */
+int main(void)
+{
+	int b = 0;
+
+	for (; b < 10; b++)
+	{
+		putchar((b % 10) + '0');
+	if (b < 10)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+
+
+}
