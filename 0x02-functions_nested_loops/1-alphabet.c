c9a229e20cea95816feb023eabb1e8387a331602
Habtu32 <habtuwendifraw@gmail.com>
2022-12-18 16:27:19 -0800

    second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print alphabet -print all the alphabet in lower case
+*
+*Return:void
+*/
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x < 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
