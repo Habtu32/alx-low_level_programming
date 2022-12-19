34b648f43a37021c10e18c763904944637249434
Habtu32 <habtuwendifraw@gmail.com>
2022-12-18 18:37:23 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - Determines if the input number
+*
+* @t: The input number as an integer.
+*
+* Return: absolute value
+*/
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
