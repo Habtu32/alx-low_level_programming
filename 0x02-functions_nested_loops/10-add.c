6fe7f2385f38d55748d8000f238e28e2d5fd130d
Habtu32 <habtuwendifraw@gmail.com>
2022-12-19 18:36:17 -0800

    add teo numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - a function that adds two integers
+* @a: number one.
+* @b: number two.
+*
+* Return: Add of number one and number two.
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
