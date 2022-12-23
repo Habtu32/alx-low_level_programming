abe0931005a3acee982b733bbc717e224b389d50
Habtu32 <habtuwendifraw@gmail.com>
2022-12-23 05:31:46 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+/**
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long hab1 = 0, hab2 = 1, habsum;
+	float habtotsum;
+
+	while (1)
+	{
+		habsum = hab1 + hab2;
+		if (habsum > 4000000)
+			break;
+
+		if ((habsum % 2) == 0)
+			habtotsum += habsum;
+
+		hab1 = hab2;
+		hab2 = habsum;
+	}
+	printf("%.0f\n", habtotsum);
+
+	return (0);
+}
