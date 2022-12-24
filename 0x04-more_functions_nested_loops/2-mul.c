869583929f7b4cc6d42151f34e670ba17aa2f5be
Habtu32 <habtuwendifraw@gmail.com>
2022-12-24 02:24:55 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - multiplication of two mumbers
+ *@a: fro intger a
+ *@b: for intiger b
+ *Return: x
+ */
+	int mul(int a, int b)
+	{
+	int x;
+
+	x = a + b;
+	return (x);
+
+	}
