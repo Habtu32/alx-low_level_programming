857a4776ed392d2f8862b7245978d9d718e71ab0
Habtu32 <habtuwendifraw@gmail.com>
2022-12-30 00:41:54 -0800

    task 2 updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,19 @@
 void print_alphabet_x10(void)
 {
 
-	char c = 'a';
 	int i = 1;
 
 	while (i <= 10)
 	{
+		char c= 'a';
+
 		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
 		}
+		putchar('\n');
+
 		i++;
 	}
 }
