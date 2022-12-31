506476349f532b98a8250f3e25ff88d948d0eedd
Habtu32 <habtuwendifraw@gmail.com>
2022-12-30 23:27:39 -0800

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,17 +9,17 @@
 int _abs(int n)
 {
 
-	if (n <= 0)
+	if (n < 0)
 	{
-		_putchar('n');
+		_putchar(n);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(0);
 	}
 	else
 	{
-		_putchar('n');
+		_putchar(n);
 	}
 
 return 0;
