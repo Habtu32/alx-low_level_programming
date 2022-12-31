a801e1939677c3230e605ff86882b28dbb8e6603
Habtu32 <habtuwendifraw@gmail.com>
2022-12-31 07:47:22 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,16 @@
  */
 void times_table(void)
 {
-	int x = 48;
+	int x = 0;
 	int y;
 
-	while (x <= 57)
+	while (x <= 9)
 	{
-		y = 48;
+		y = 0;
 
-		while (y <= 57)
+		while (y <= 9)
 		{
-			_putchar((x * y) + '0');
+			_putchar((x * y) + 48);
 			_putchar(',');
 			_putchar(' ');
 
