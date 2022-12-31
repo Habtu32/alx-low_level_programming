af7bbf10079fcb8486f33181f4fb77531c4de17b
Habtu32 <habtuwendifraw@gmail.com>
2022-12-30 16:38:21 -0800

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,7 +7,7 @@
  */
 int _islower(int c)
 {
-	if ((c >= 97) && (c <= 122))
+	if ((c >= 'a') && (c <= 'z'))
 	{
 		return (1);
 	}
@@ -15,6 +15,5 @@ int _islower(int c)
 	{
 		return (0);
 	}
-	
 return (0);
 }
