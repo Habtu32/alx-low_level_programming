af95ee6b07d8e4e72bb6986ea1f0244be5b0ecf8
Habtu32 <habtuwendifraw@gmail.com>
2023-01-01 00:30:58 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,42 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - print natural number
  * @n:to print natural number
  * Return:
- */ 
+ */
 void print_to_98(int n)
 {
-	n = 0;
 
-	while (n <= 98)
+	if (n <= 98)
 	{
-		if (n <= 9)
+		for (n = n; n <= 98; n++)
 		{
-			_putchar(n + 48);
+			if (n <= 9)
+			{
+				_putchar(n + 48);
+			}
+			else
+			{
+				_putchar(n / 10 + 48);
+				_putchar(n % 10 + 48);
+
+			}
 		}
-		else
+	}
+	else if (n > 98)
+	{
+		for (n = n; n >= 98; n--)
 		{
 			_putchar(n / 10 + 48);
-			_putchar(n % 10 + 48);
+			_putchar(n / 10 + 48);
 		}
-		if (n != 98)
-		{
-			_putchar(44);
-			_putchar(32);
-		}
-		n++;
 
+	}
+	if (n != 98)
+	{
+	_putchar(44);
+	_putchar(32);
 	}
 	_putchar('\n');
 }
