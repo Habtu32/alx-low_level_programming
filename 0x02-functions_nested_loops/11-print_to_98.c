c634607d8b47ebdc799a349df4a82b46d24842af
Habtu32 <habtuwendifraw@gmail.com>
2023-01-01 01:34:06 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,24 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
+
+			printf("%d", n);
+
 			if (n != 98)
-			printf("%d, ", n);
+			_putchar(44);
+			_putchar(32);
+
 		}
 	}
 	else if (n > 98)
 	{
 		for (n = n; n >= 98; n--)
 		{
+			printf("%d", n);
+
 			if (n != 98)
-			printf("%d, ", n);
+			_putchar(44);
+			_putchar(32);
 		}
 
 	}
