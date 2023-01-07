653b14b849529aff9bc1b460fe10e6f68b13d894
habtuwendifraw@gmail.com <habtuwendifraw@gmail.com>
2023-01-07 11:12:54 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,28 @@
  */
 void rev_string(char *s)
 {
-	int len;
+	int len = 0;
+	int temp, middle;
 
-	for (len = 0; s[len] != '\0'; len++)
+	while (*s != '\0')
 	{
-	
+		len++;
+
+		s++;
 	}
-	for (len = len - 1; len >= 0; len--)
+
+	middle = len / 2;
+
+	for (int i = 0; i < middle; i++)
 	{
-		_putchar(s[len]);
+		temp = s[i];
+
+		s[i] = s[len - i - 1];
+
+		s[len - i - 1] = temp;
+
+		_putchar(s[i]);
+
 	}
 
 }
