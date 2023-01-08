5299bbab68a60bf14947c631a16693ade0b2b181
habtuwendifraw@gmail.com <habtuwendifraw@gmail.com>
2023-01-08 12:02:21 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,17 +2,22 @@
 #include<stdio.h>
 /**
  *puts_half - print char
- *
  *@str: string print
  *Return: no value
  */
 void puts_half(char *str)
 {
-	int i, j, mid;
+	int i, j, i;
+	float mid;
 
 	for (i = 0; str[i] != '\0'; i++)
 
 	mid = i / 2;
+
+	if (mid % 2 != 0)
+	{
+		mid = (i - 1) / 2;
+	}
 
 	for (j = mid; j <= i; j--)
 	{
