0db7eee2a865642a296c0bd8f1d77e21e4c88cd9
habtuwendifraw@gmail.com <habtuwendifraw@gmail.com>
2023-01-08 13:28:49 -0800

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,16 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
+		if (i != n)
+		{
 		printf("%d", a[i]);
 		printf(",");
 		printf(" ");
+		}
+		else
+		{
+		printf("%d", a[i]);
+		}
 	}
 	printf("\n");
 }
