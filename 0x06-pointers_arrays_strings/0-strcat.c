ebe233f85da206bcc6f1c8f8ff0541c8bf62f9e7
habtuwendifraw@gmail.com <habtuwendifraw@gmail.com>
2023-01-09 02:00:38 -0800

    Task o

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int len, i, j;
 
-	i = 0;
-	while (dest[i] != '\0')
+	for (i = 0; i < dest[i] != '\0'; i++)
 	{
-		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	len = i;
+
+	for (j = 0; j < src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[len] = src[j];
+
+		len++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	dest[len] = '\0';
+
+return (dest);
 }
