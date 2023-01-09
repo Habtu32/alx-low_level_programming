b3833f8f2715be94dc38ecd985768958c0fa4041
habtuwendifraw@gmail.com <habtuwendifraw@gmail.com>
2023-01-08 23:56:47 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,10 +15,11 @@ char *_strcpy(char *dest, char *src)
 	{
 	}
 
-	for (j = 0; j <= i && src[j] != '\0'; j++)
+	for (j = 0; j <= i; j++)
 	{
 		dest[j] = src[j];
 	}
+	dest[j] = '\0';
 
 return (dest);
 }
